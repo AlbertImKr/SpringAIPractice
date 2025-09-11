a0d950a95b7168f4fb9b79597c3ca8f04690c4d5
ALbertIM0427 <guang7226961@naver.com>
2025-09-11 17:20:14 +0900

    chore: project init

---
+++ src/main/kotlin/com/albert/springaipractice/SpringAiPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.albert.springaipractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAiPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringAiPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/albert/springaipractice/SpringAiPracticeApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.albert.springaipractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAiPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
